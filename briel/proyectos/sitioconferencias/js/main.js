32b68283f1b082bf283336727fa27e4e4671c3fc
gabrielvejar <g.vejarperez@gmail.com>
2019-09-03 22:54:15 -0400

    asdf

--- briel/proyectos/sitioconferencias/js/main.js
+++ briel/proyectos/sitioconferencias/js/main.js
@@ -6,15 +6,15 @@
     document.addEventListener('DOMContentLoaded', function(){
 
 
-        var map = L.map('mapa').setView([-33.443203, -70.655852], 16);
+        // var map = L.map('mapa').setView([-33.443203, -70.655852], 16);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        // }).addTo(map);
 
-        L.marker([-33.443203, -70.655852]).addTo(map)
-            .bindPopup('GDLWEBCAMP<br>Entradas ya a la venta.<br><a target="_blank" href="https://goo.gl/maps/MK1qQJr2c9vJSTDT7">Abrir en Google Maps</a>')
-            .openPopup();
+        // L.marker([-33.443203, -70.655852]).addTo(map)
+        //     .bindPopup('GDLWEBCAMP<br>Entradas ya a la venta.<br><a target="_blank" href="https://goo.gl/maps/MK1qQJr2c9vJSTDT7">Abrir en Google Maps</a>')
+        //     .openPopup();
 
         //Campos Datos usuario
         var nombre = document.getElementById('nombre');
@@ -83,6 +83,23 @@
                 for (var i = 0; i < listadoProductos.length; i++){
                     lista_productos.innerHTML += listadoProductos[i] + '</br>';
                 }
+
+                var regaloSeleccionado = regalo.value;
+                switch(regaloSeleccionado){
+                    case 'ETI': 
+                        regaloSeleccionado = 'Etiquetas';
+                        break;
+                    case 'PUL': 
+                        regaloSeleccionado = 'Pulsera';
+                        break;
+                    case 'PLU': 
+                        regaloSeleccionado = 'Plumas';
+                        break;
+                }
+
+                lista_productos.innerHTML+='Regalo: '+ regaloSeleccionado;
+
+
                 suma.innerHTML = "$ " + totalPagar.toFixed(0);
                 
             }
@@ -133,3 +150,71 @@
     }); // DOM Content Loaded
 
 })();
+
+
+$(function() {
+
+
+    //Menú fijo
+     var windowHeight = $(window).height();
+         var barraAltura = $('.barra').innerHeight();
+
+
+
+    $(window).scroll(function(){
+
+        var scroll = $(window).scrollTop();
+
+        if (scroll > windowHeight){
+            $('.barra').addClass('fixed');
+            $('body').css({'margin-top' : barraAltura+'px'});
+        }else{
+            $('.barra').removeClass('fixed');
+            $('body').css({'margin-top' : '0px'});
+        }
+        
+    });
+
+
+    //Lettering
+    $('.nombre-sitio').lettering();
+
+
+    //Programa de conferencias
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+    $('.menu-programa a').on('click', function(){
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+    });
+
+    //Animacion para los numeros
+
+    var resumenLista = $('.resumen-evento');
+    if (resumenLista.length > 0){
+        $('.resumen-evento').waypoint(function() {
+        
+            $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6} , 1200);
+            $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15} , 1200);
+            $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3} , 1700);
+            $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9} , 1400);
+
+        }, {
+            offset: '60%'
+        }) ;  
+    }
+
+    //Animacion cuenta regresiva
+
+    $('.cuenta-regresiva').countdown('2019/12/10 09:00:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    })
+
+});
