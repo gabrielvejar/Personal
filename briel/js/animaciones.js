61df8da86141ae5eeb51c4a3eb9543e1759126eb
gabrielvejar <g.vejarperez@gmail.com>
2019-09-06 20:53:23 -0400

    cambio estilos

--- briel/js/animaciones.js
+++ briel/js/animaciones.js
@@ -1,7 +1,18 @@
 $('#ventana-contacto').hide();
 
 
-var titulos_gabriel = new Typed('#titulos-gabriel', {
+if(window.innerWidth<=640){
+    document.querySelector('#iconos').setAttribute("data-aos" , "slide-down");
+} else {
+    document.querySelector('#iconos').setAttribute("data-aos" , "slide-right");
+}
+
+
+$(function(){
+
+    
+    
+    var titulos_gabriel = new Typed('#titulos-gabriel', {
     // strings: ['engen' , 'enginier' , 'enge' , 'engine' , 'e^2000' , 'ingeniero', 'web designer' , 'JavaScript programmer' , 'PHP programmer' , 'Java programmer' , 'Python programmer', 'and more...^2000' , '1313' ],
     strings: ['Ingeniero en Telecomunicaciones,<br> Conectividad y Redes' , 'Desarrollador Web' , 'Programador JavaScript' , 'Programador PHP' , 'Programador Java' , 'Programador Python'],
     // strings: ['Ingeniero en Telecomunicaciones, Conectividad y Redes' , 'Code enthusiast'],
@@ -24,12 +35,12 @@ typed1.stop();
 
 
 var typed2 = new Typed('#typed-formacion', {
-stringsElement: '#texto-formacion',
-typeSpeed: 0,
-startDelay: 1000,
-loop: false,
-showCursor: true,
-fadeOut: true
+    stringsElement: '#texto-formacion',
+    typeSpeed: 0,
+    startDelay: 1000,
+    loop: false,
+    showCursor: true,
+    fadeOut: true
 });
 typed2.stop();
 
@@ -37,59 +48,77 @@ typed2.stop();
 
 document.addEventListener('aos:in:info', ({ detail }) => {
     typed1.start();
-
+    
 });
 
 document.addEventListener('aos:in:formacion', ({ detail }) => {
     typed2.start();
-
+    
 });
 
 
 document.addEventListener('aos:in:contacto', ({ detail }) => {
     
-
+    
 });
 
 
 
 $('#icono-inicio').addClass('borde');
+$('#icono-inicio').addClass('activo');
+
 
 window.addEventListener('scroll', () => {
-
+    
     $('#ventana-contacto').show();
     const scrolled = window.scrollY;
-
+    
 
     //main - inicio
     if (scrolled < window.innerHeight) {
         // ventana('texto-principal');
         $('.clase-iconos').removeClass('borde');
         $('#icono-inicio').addClass('borde');
+        $('.clase-iconos').removeClass('activo');
+        $('#icono-inicio').addClass('activo');
     }
-
+    
     //seccion info personal
     if (scrolled >= window.innerHeight*.75 && scrolled < window.innerHeight*1.75){
-
-            $('.clase-iconos').removeClass('borde');
-            $('#icono-info-personal').addClass('borde');
+        
+        $('.clase-iconos').removeClass('borde');
+        $('#icono-info-personal').addClass('borde');
+        $('.clase-iconos').removeClass('activo');
+        $('#icono-info-personal').addClass('activo');
     }
-
+    
     //seccion formacion academica
     if (scrolled >= window.innerHeight*1.75 && scrolled < window.innerHeight*2.75) {
-
+        
         $('.clase-iconos').removeClass('borde');
         $('#icono-formacion').addClass('borde');
-
+        $('.clase-iconos').removeClass('activo');
+        $('#icono-formacion').addClass('activo');
+        
     }
-
+    
     //seccion contacto
     if (scrolled >= window.innerHeight*2.75 && scrolled < window.innerHeight*3.75) {
-
+        
         $('.clase-iconos').removeClass('borde');
         $('#icono-contacto').addClass('borde');
-
-
+        $('.clase-iconos').removeClass('activo');
+        $('#icono-contacto').addClass('activo');
+        
+        
     }
+    
+});
+
+
+
 
 });
+
+
+
